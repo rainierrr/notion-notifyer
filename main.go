e82b46f5ed5ecc30e783781477f680e6cebd2984
石黒湧大 <yudai.ishiguro.it@gmail.com>
2025-05-01 23:50:17 +0900

    add basci filter and sort function

--- main.go
+++ main.go
@@ -19,12 +19,48 @@ func main() {
 	dbID := "70c9526ec8e54c7b9ebbe48f21c4c429"
 	ctx := context.Background()
 	request := &notionapi.DatabaseQueryRequest{
-		// Filter:   &notionapi.PropertyFilter{},
+		// TODO: 追加でフィルターを増やす
+		Filter: &notionapi.OrCompoundFilter{
+			&notionapi.PropertyFilter{
+				Property: "Schedule Status",
+				Status: &notionapi.StatusFilterCondition{
+					Equals: "Friday",
+				},
+			},
+			&notionapi.PropertyFilter{
+				Property: "Type",
+				Select: &notionapi.SelectFilterCondition{
+					Equals: "Monday",
+				},
+			},
+		},
+		// TODO: Sortが正常に動いているかチェック
+		Sorts: []notionapi.SortObject{
+			{
+				Property:  "Due",
+				Direction: "descending",
+			},
+		},
 		PageSize: 10,
 	}
 	page, err := client.Database.Query(ctx, notionapi.DatabaseID(dbID), request)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%+v", page)
+	for _, page := range page.Results {
+
+		title := page.Properties["Name"].(*notionapi.TitleProperty).Title[0].Text.Content
+		// start_time := page.Properties["Due"].(*notionapi.DateProperty).Date.Start
+		// end_time := page.Properties["Due"].(*notionapi.DateProperty).Date.End
+		priority := page.Properties["Priority"].(*notionapi.SelectProperty).Select.Name
+		task_type := page.Properties["Type"].(*notionapi.SelectProperty).Select.Name
+		// memo := page.Properties["Memo"].(*notionapi.RichTextProperty).RichText[0].Text.Content
+
+		fmt.Printf("Title: %s\n", title)
+		// fmt.Printf("Due Date: %s - %s\n", start_time, end_time)
+		fmt.Printf("Priority: %s\n", priority)
+		fmt.Printf("Type: %s\n", task_type)
+		// fmt.Printf("Memo: %s\n", memo)
+		fmt.Println()
+	}
 }

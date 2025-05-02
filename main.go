12483e6ccaaace970cf5ea1b946a13c7e7b873db
石黒湧大 <yudai.ishiguro.it@gmail.com>
2025-05-02 23:33:55 +0900

    fix filter and sort

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jomei/notionapi"
 )
@@ -16,32 +17,51 @@ func main() {
 	}
 
 	client := notionapi.NewClient(notionapi.Token(NOTION_TOKEN))
+	// タスクDBのDB ID
 	dbID := "70c9526ec8e54c7b9ebbe48f21c4c429"
+
 	ctx := context.Background()
+
+	threeDay := time.Now().AddDate(0, 0, 3)
 	request := &notionapi.DatabaseQueryRequest{
-		// TODO: 追加でフィルターを増やす
-		Filter: &notionapi.OrCompoundFilter{
+		// 今より3日後以前のタスクを取得
+		Filter: &notionapi.AndCompoundFilter{
 			&notionapi.PropertyFilter{
-				Property: "Schedule Status",
-				Status: &notionapi.StatusFilterCondition{
-					Equals: "Friday",
+				Property: "Due",
+				Date: &notionapi.DateFilterCondition{
+					Before: (*notionapi.Date)(&threeDay),
 				},
 			},
-			&notionapi.PropertyFilter{
-				Property: "Type",
-				Select: &notionapi.SelectFilterCondition{
-					Equals: "Monday",
-				},
-			},
+			// Schedule Statusが"CannotDo", "Next", "Want", "ToDo", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"のいずれかであること
+			func() notionapi.OrCompoundFilter {
+				statuses := []string{"CannotDo", "Next", "Want", "ToDo", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+				var filter []notionapi.Filter
+				for _, status := range statuses {
+					filter = append(filter, &notionapi.PropertyFilter{
+						Property: "Schedule Status",
+						Status: &notionapi.StatusFilterCondition{
+							Equals: status,
+						},
+					})
+				}
+				orFilter := notionapi.OrCompoundFilter(filter)
+				return orFilter
+			}(),
 		},
-		// TODO: Sortが正常に動いているかチェック
 		Sorts: []notionapi.SortObject{
+			// Dueの昇順
 			{
 				Property:  "Due",
-				Direction: "descending",
+				Direction: "ascending",
+			},
+			// Priorityの昇順
+			// これは、Priorityが"High"、"Medium"、"Low"の順に並ぶようにするため
+			{
+				Property:  "Priority",
+				Direction: "ascending",
 			},
 		},
-		PageSize: 10,
+		PageSize: 20,
 	}
 	page, err := client.Database.Query(ctx, notionapi.DatabaseID(dbID), request)
 	if err != nil {
@@ -50,14 +70,22 @@ func main() {
 	for _, page := range page.Results {
 
 		title := page.Properties["Name"].(*notionapi.TitleProperty).Title[0].Text.Content
-		// start_time := page.Properties["Due"].(*notionapi.DateProperty).Date.Start
-		// end_time := page.Properties["Due"].(*notionapi.DateProperty).Date.End
+
+		var start_time *notionapi.Date
+		var end_time *notionapi.Date
+		if page.Properties["Due"].(*notionapi.DateProperty).Date != nil {
+			start_time = page.Properties["Due"].(*notionapi.DateProperty).Date.Start
+		}
+
+		if page.Properties["Due"].(*notionapi.DateProperty).Date != nil {
+			end_time = page.Properties["Due"].(*notionapi.DateProperty).Date.End
+		}
 		priority := page.Properties["Priority"].(*notionapi.SelectProperty).Select.Name
 		task_type := page.Properties["Type"].(*notionapi.SelectProperty).Select.Name
 		// memo := page.Properties["Memo"].(*notionapi.RichTextProperty).RichText[0].Text.Content
 
 		fmt.Printf("Title: %s\n", title)
-		// fmt.Printf("Due Date: %s - %s\n", start_time, end_time)
+		fmt.Printf("Due Date: %s - %s\n", start_time, end_time)
 		fmt.Printf("Priority: %s\n", priority)
 		fmt.Printf("Type: %s\n", task_type)
 		// fmt.Printf("Memo: %s\n", memo)
